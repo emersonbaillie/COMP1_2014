75130d6280d1229f5c19487dcfbe80dcfa934407
emersonbaillie <ebaillie1@gmail.com>
2014-04-15 17:15:36 +0100

    Task 4 - Run 2

--- Skeleton Program.py
+++ Skeleton Program.py
@@ -169,10 +169,10 @@ def DisplayRecentScores(RecentScores):
   print()
   print('Recent Scores: ')
   print()
+  print("{0:>7}{1:>7}".format("Name","Score"))
+  print()
   for Count in range(1, NO_OF_RECENT_SCORES + 1):
-    print("{0}{1:>7}".format("Name","Score"))
-    print()
-    print("{0}{1:>7}".format(RecentScores[Count].Name,RecentScores[Count].Score))
+    print("{0:>7}{1:>7}".format(RecentScores[Count].Name,RecentScores[Count].Score))
   print()
   print('Press the Enter key to return to the main menu')
   input()
