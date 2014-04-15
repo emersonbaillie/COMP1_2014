a29114cc22d70395a6af0cd5b7f4e0cc3cca50d4
emersonbaillie <ebaillie1@gmail.com>
2014-04-15 17:14:17 +0100

    Task 4 - ScoreBoard first run
    Task 4 first run

--- Skeleton Program.py
+++ Skeleton Program.py
@@ -170,7 +170,9 @@ def DisplayRecentScores(RecentScores):
   print('Recent Scores: ')
   print()
   for Count in range(1, NO_OF_RECENT_SCORES + 1):
-    print(RecentScores[Count].Name, 'got a score of', RecentScores[Count].Score)
+    print("{0}{1:>7}".format("Name","Score"))
+    print()
+    print("{0}{1:>7}".format(RecentScores[Count].Name,RecentScores[Count].Score))
   print()
   print('Press the Enter key to return to the main menu')
   input()
