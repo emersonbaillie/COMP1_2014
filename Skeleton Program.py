d2aa8cee5959c23cb45ce49a94910a1ccb3af66c
emersonbaillie <ebaillie1@gmail.com>
2014-04-15 17:04:33 +0100

    Task 3b - Save score?
    Asks user if score should be saved. Strips to first letter and
    capitalizes the letter. Also gives user feedback to whether or not the
    score was/wasn't saved

--- Skeleton Program.py
+++ Skeleton Program.py
@@ -177,21 +177,28 @@ def DisplayRecentScores(RecentScores):
   print()
 
 def UpdateRecentScores(RecentScores, Score):
-  PlayerName = GetPlayerName()
-  FoundSpace = False
-  Count = 1
-  while (not FoundSpace) and (Count <= NO_OF_RECENT_SCORES):
-    if RecentScores[Count].Name == '':
-      FoundSpace = True
-    else:
-      Count = Count + 1
-  if not FoundSpace:
-    for Count in range(1, NO_OF_RECENT_SCORES):
-      RecentScores[Count].Name = RecentScores[Count + 1].Name
-      RecentScores[Count].Score = RecentScores[Count + 1].Score
-    Count = NO_OF_RECENT_SCORES
-  RecentScores[Count].Name = PlayerName
-  RecentScores[Count].Score = Score
+  SaveScore = input("Do you wish to save your score? (Y/N):")
+  SaveScore = SaveScore[0].upper()
+  if SaveScore != "N":
+    PlayerName = GetPlayerName()
+    print("Score Saved")
+    FoundSpace = False
+    Count = 1
+    while (not FoundSpace) and (Count <= NO_OF_RECENT_SCORES):
+      if RecentScores[Count].Name == '':
+        FoundSpace = True
+      else:
+        Count = Count + 1
+    if not FoundSpace:
+      for Count in range(1, NO_OF_RECENT_SCORES):
+        RecentScores[Count].Name = RecentScores[Count + 1].Name
+        RecentScores[Count].Score = RecentScores[Count + 1].Score
+      Count = NO_OF_RECENT_SCORES
+    RecentScores[Count].Name = PlayerName
+    RecentScores[Count].Score = Score
+  else:
+    print()
+    print("Score not saved")
 
 def PlayGame(Deck, RecentScores):
   LastCard = TCard()
